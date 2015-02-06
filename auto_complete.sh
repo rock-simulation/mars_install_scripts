a8dad627659cad6a7940eab3ddff04b43016d67c
Malte Langosz <malte.langosz@dfki.de>
2015-02-06 15:06:55 +0100

    added auto complete functionallity for mars install script

---
+++ auto_complete.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+SOURCES_FILE="sources.txt"
+
+MARS_SCRIPT_DIR="."
+
+function parse_yaml {
+   local s='[[:space:]]*' w='[a-zA-Z0-9_-]*' fs=$(echo @|tr @ '\034')
+   sed -ne "/.:./d" \
+        -e "/#/d" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\2|p" $1
+}
+
+packages1="bootstrap install rebuild clean"
+packages=""
+while read source_file; do
+    source_file=${source_file/\#*/}
+    if [[ x${source_file} = x ]]; then
+        continue
+    fi
+    echo "parse ${source_file}"
+    p=$(parse_yaml "${source_file}")
+    IFS='
+'
+    set -f
+    for line in ${p}; do
+	if [[ x${packages} != x ]]; then
+	    packages+=" "
+	fi
+	packages+=${line}
+    done
+    set +f
+    unset IFS
+done < ${MARS_SCRIPT_DIR}/${SOURCES_FILE}
+
+echo "packages: ${packages1} ${packages}"
+
+complete -o default -W "${packages1} ${packages}" mars.sh
+complete -o default -W "${packages}" mars_install
+complete -o default -W "${packages}" mars_bootstrap
+complete -o default -W "${packages}" mars_rebuild
