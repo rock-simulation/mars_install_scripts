1f257b191bc3dfe3f2bee8435920445265407cfb
Kai von Szadkowski <kai.von-szadkowski@uni-bremen.de>
2015-02-13 10:33:31 +0100

    add tinyxml dependency to install scripts

--- apt_get_dep.sh
+++ apt_get_dep.sh
@@ -1,3 +1,3 @@
-sudo apt-get install cmake libopenscenegraph-dev libqt4-dev zlib1g-dev libcvaux-dev libhighgui-dev libopencv-dev libqt4-opengl libqt4-opengl-dev 
+sudo apt-get install cmake libopenscenegraph-dev libqt4-dev zlib1g-dev libcvaux-dev libhighgui-dev libopencv-dev libqt4-opengl libqt4-opengl-dev libtinyxml-dev
 #libcv-dev
 #libxmu-dev libxi-dev
